47ce651aeb3a1fbb9d4e804defb4962dd7fb7440
Mathew O'Dwyer <mathew.odwyer@outlook.com>
2024-01-07 14:15:36 +1100

    woot woot

--- ShadowMapping/ShadowMapping/GameContainer.cs
+++ ShadowMapping/ShadowMapping/GameContainer.cs
@@ -12,18 +12,19 @@ using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
+//// TODO: Move all of this into Final Engine
+//// TODO: Add cascaded shadow mapping to resolve issue with directional lighting.
+
 public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
     : GameWindow(gameWindowSettings, nativeWindowSettings)
 {
-    private const int ShadowHeight = 2048;
+    private const int ShadowHeight = 8192;
 
-    private const int ShadowWidth = 2048;
+    private const int ShadowWidth = 8192;
 
     private Camera camera;
 
     private ImGuiController controller;
-
-    private int cubeVAO;
 
     private ShaderProgram debugShader;
 
@@ -41,17 +42,11 @@ public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeW
 
     private ModelResource modelResource;
 
-    private int planeVAO;
-
-    private int quadVAO;
-
-    private int quadVBO;
-
     private ShaderProgram shader;
 
     private float temp = 0;
 
-    private Texture woodTexture;
+    private Texture texture;
 
     protected override void OnLoad()
     {
@@ -67,89 +62,7 @@ public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeW
             File.ReadAllText("Resources\\Shaders\\debug.vert"),
             File.ReadAllText("Resources\\Shaders\\debug.frag"));
 
-        float[] planeVertices =
-        {
-             25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
-            -25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
-            -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
-
-             25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
-            -25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
-             25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f,
-        };
-
-        float[] cubeVertices = {
-			// back face
-			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
-			 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
-			 1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 0.0f, // bottom-right
-			 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 1.0f, 1.0f, // top-right
-			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
-			-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f, 0.0f, 1.0f, // top-left
-			// front face
-			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
-			 1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 0.0f, // bottom-right
-			 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
-			 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f, 1.0f, // top-right
-			-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 1.0f, // top-left
-			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom-left
-			// left face
-			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
-			-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-left
-			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
-			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-left
-			-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-right
-			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-right
-			// right face
-			 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
-			 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
-			 1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 1.0f, // top-right
-			 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 1.0f, // bottom-right
-			 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 1.0f, 0.0f, // top-left
-			 1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, 0.0f, 0.0f, // bottom-left
-			 // bottom face
-			 -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
-			  1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 1.0f, // top-left
-			  1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
-			  1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 1.0f, 0.0f, // bottom-left
-			 -1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 0.0f, // bottom-right
-			 -1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, 0.0f, 1.0f, // top-right
-			 // top face
-			 -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
-			  1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
-			  1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 1.0f, // top-right
-			  1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f, // bottom-right
-			 -1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f, // top-left
-			 -1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f  // bottom-left
-		};
-
-        float[] quadVertices = {
-            -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
-            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-             1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
-             1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-        };
-
-        this.quadVAO = GL.GenVertexArray();
-        this.quadVBO = GL.GenBuffer();
-
-        GL.BindVertexArray(quadVAO);
-
-        GL.BindBuffer(BufferTarget.ArrayBuffer, quadVBO);
-        GL.BufferData(BufferTarget.ArrayBuffer, quadVertices.Length * sizeof(float), quadVertices, BufferUsageHint.StaticDraw);
-
-        GL.EnableVertexAttribArray(0);
-        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
-
-        GL.EnableVertexAttribArray(1);
-        GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
-
-        GL.BindVertexArray(0);
-
-        this.planeVAO = CreateMesh(planeVertices);
-        this.cubeVAO = CreateMesh(cubeVertices);
-
-        this.woodTexture = Texture.LoadTexture("Resources\\Textures\\texture.png");
+        this.texture = Texture.LoadTexture("Resources\\Textures\\texture.png");
 
         this.depthMapFBO = GL.GenFramebuffer();
         this.depthMap = GL.GenTexture();
@@ -157,8 +70,8 @@ public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeW
         GL.BindTexture(TextureTarget.Texture2D, this.depthMap);
         GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.DepthComponent, ShadowWidth, ShadowHeight, 0, PixelFormat.DepthComponent, PixelType.Float, IntPtr.Zero);
 
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
-        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMinFilter.Nearest);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMinFilter.Linear);
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.ClampToBorder);
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.ClampToBorder);
 
@@ -188,8 +101,10 @@ public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeW
 
         this.loader = new ModelLoader();
 
-        this.modelResource = this.loader.LoadResource("Resources\\Models\\Sponza\\sponza.obj");
         this.controller = new ImGuiController(this, ClientSize.X, ClientSize.Y);
+
+        this.modelResource = loader.LoadResource("Resources\\Models\\Sponza\\sponza.obj");
+
         base.OnLoad();
     }
 
@@ -198,11 +113,6 @@ public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeW
         GL.Enable(EnableCap.DepthTest);
         GL.ClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
-        var lightTransform = new Transform()
-        {
-            Rotation = Quaternion.FromAxisAngle(Vector3.UnitX, MathHelper.DegreesToRadians(45.0f)),
-        };
 
         var lightProjection = Matrix4.CreateOrthographicOffCenter(-40, 40, -40, 40, -40, 40);
         var lightView = Matrix4.LookAt(lightPosition, Vector3.Zero, Vector3.UnitY);
@@ -216,12 +126,9 @@ public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeW
 
         GL.Clear(ClearBufferMask.DepthBufferBit);
 
-        GL.Enable(EnableCap.CullFace);
         GL.CullFace(CullFaceMode.Front);
 
         this.RenderScene(this.depthShader);
-
-        GL.Disable(EnableCap.CullFace);
 
         GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
         GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
@@ -267,35 +174,9 @@ public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeW
         base.OnUpdateFrame(args);
     }
 
-    private int CreateMesh(float[] vertices)
-    {
-        int vao = GL.GenVertexArray();
-        int vbo = GL.GenBuffer();
-
-        GL.BindVertexArray(vao);
-
-        GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-        GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-
-        GL.EnableVertexAttribArray(0);
-        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 0);
-
-        GL.EnableVertexAttribArray(1);
-        GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 3 * sizeof(float));
-
-        GL.EnableVertexAttribArray(2);
-        GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), 6 * sizeof(float));
-
-        GL.BindVertexArray(0);
-
-        return vao;
-    }
-
     private void RenderScene(ShaderProgram shader)
     {
-        this.woodTexture.Bind(0);
-
-        var model = Matrix4.CreateScale(1f);
+        var model = Matrix4.CreateScale(0.01f);
         shader.SetMatrix4("model", model);
 
         foreach (var res in modelResource.Models)
@@ -305,7 +186,7 @@ public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeW
 
             if (material == null)
             {
-                woodTexture.Bind(0);
+                texture.Bind(0);
             }
             else
             {
@@ -314,22 +195,5 @@ public sealed class GameContainer(GameWindowSettings gameWindowSettings, NativeW
 
             mesh.Draw();
         }
-
-        //GL.BindVertexArray(this.planeVAO);
-        //GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
-
-        //model = Matrix4.Identity;
-        //model *= Matrix4.CreateTranslation(0, 1.5f, 0) * Matrix4.CreateScale(0.5f);
-        //shader.SetMatrix4("model", model);
-
-        //GL.BindVertexArray(this.cubeVAO);
-        //GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
-
-        //model = Matrix4.Identity;
-        //model *= Matrix4.CreateTranslation(10.0f, 4, 1.0f) * Matrix4.CreateScale(0.5f);
-        //shader.SetMatrix4("model", model);
-
-        //GL.BindVertexArray(this.cubeVAO);
-        //GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
     }
 }
--- ShadowMapping/ShadowMapping/Resources/Shaders/shader.frag
+++ ShadowMapping/ShadowMapping/Resources/Shaders/shader.frag
@@ -29,6 +29,7 @@ float ShadowCalculation(vec4 fragPosLightSpace)
     vec3 normal = normalize(fs_in.Normal);
     vec3 lightDir = normalize(lightPos - fs_in.FragPos);
     float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
+    
     // check whether current frag pos is in shadow
     // float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
     // PCF
@@ -52,7 +53,7 @@ float ShadowCalculation(vec4 fragPosLightSpace)
 }
 
 void main()
-{           
+{
     vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
     vec3 normal = normalize(fs_in.Normal);
     // ambient
